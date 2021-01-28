74ade470d04f2bfc95551a18c63793792af22895
AkashevPavel <pavela29@gmail.com>
2021-01-28 04:30:02 +0300

    initial commit

---
+++ app/src/main/java/com/example/mymemorygame/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mymemorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemorygame.models.BoardSize
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard : RecyclerView
+    private lateinit var tvNumMoves : TextView
+    private lateinit var tvNumPairs : TextView
+
+    private val boardSize: BoardSize = BoardSize.EASY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+}
